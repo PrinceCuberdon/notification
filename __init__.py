5b913f0980a64766876ea55d8617abd6e5ac02cc
regisf <regisfloret@gmail.com>
2016-02-07 20:16:55 +0100

    Make the tests passing following the Django 1.8 passage and a small refactorisation

--- __init__.py
+++ __init__.py
@@ -1,19 +1,7 @@
 # -*- coding: UTF-8 -*-
 # notification is part of Band Cochon
 # Band Cochon (c) Prince Cuberdon 2011 and Later <princecuberdon@bandcochon.fr>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import smtplib
 from email.mime.text import MIMEText
@@ -22,7 +10,6 @@ from email.mime.multipart import MIMEMultipart
 import sys
 from django.utils import timezone
 import os
-import datetime
 import codecs
 
 from django import template
@@ -36,15 +23,12 @@ def notification_send(template_shortcut, dest=None, context=None, account_name=N
     """ Send an email based on a shortcut """
     from .models import Preference
 
-    # When developping,
-    if settings.IS_LOCAL or settings.IS_TESTING:
-        return True
-
     try:
         if account_name is not None:
             pref = Preference.objects.get(name=account_name)
         else:
             pref = Preference.objects.get_default()
+
     except ObjectDoesNotExist:
         raise Http404()
 
@@ -54,6 +38,7 @@ def notification_send(template_shortcut, dest=None, context=None, account_name=N
         notif = Notification(template_shortcut)
         notif.push(dest, context)
         notif.send()
+
         return True
 
     except ObjectDoesNotExist as error:
@@ -75,15 +60,12 @@ class NotificationError(Exception):
 
 class Notification(object):
     def __init__(self, template_shortcut=None, debug=False):
-        if settings.IS_LOCAL or settings.IS_TESTING or debug:
-            return
-
         from .models import Preference
 
         self.notif = []
         self.shortcut = template_shortcut
         self.pref = Preference.objects.get_default()
-        if debug == False:
+        if not debug:
             self.connection = smtplib.SMTP(self.pref.server, int(self.pref.port))
             if not self.pref.anonymous:
                 self.connection.login(self.pref.username, self.pref.password)
@@ -103,8 +85,6 @@ class Notification(object):
 
     def push(self, dest, context=None, shortcut=None):
         """ Push notifications in a stack """
-        if settings.IS_LOCAL or settings.IS_TESTING:
-            return
 
         if self.shortcut is None:
             if self.subject is None or self.body is None:
@@ -131,12 +111,10 @@ class Notification(object):
             msg['From'] = self.pref.sendmail
             msg['To'] = notif['dest']
             msg['Subject'] = notif['subject']
-            # textpart = MIMEText(notif['body'],'plain', 'utf-8')
             htmlpart = MIMEText('<html><body>' + notif['body'] + '</body></html>', 'html', 'utf-8')
             msg.attach(htmlpart)
-            # msg.attach(textpart)
 
-            if debug == False:
+            if not debug:
                 try:
                     # Ensure email is a valid one
                     validate_email(notif['dest'])
@@ -173,5 +151,3 @@ def ajax_log(message, testing=False):
             print >> sys.stderr, message
     except:
         pass
-        # if Preferences.objects.filter(default_accout=True)[0].send_to_admin == True:
-        #    notification_send()
--- tests.py
+++ tests.py
@@ -1,55 +1,32 @@
 # -*- coding: UTF-8 -*-
 # notification is part of Band Cochon
 # Band Cochon (c) Prince Cuberdon 2011 and Later <princecuberdon@bandcochon.fr>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Test notification.
 
 Embed its own fake smtpd server
 """
-import os
-#import datetime
+import smtplib
 
 from django.test import TestCase
-from django.conf import settings
 from django.http import Http404
 from django.template import Context
 from django.contrib.auth.models import User
 
-from core.bandcochon.models import Utilisateur #, Picture, TownShip, Town, Place
-from .models import Preference, Template, MailingList
-from . import ajax_log, notification_send
+import minimock
 
-class TestAjaxLog(TestCase):
-    def setUp(self):
-        """ Clean up the ajax_log.txt file """
-        open(os.path.join(settings.MEDIA_ROOT, "ajax_log.txt"),"w").write('')
-        
-    def test_ajax_log(self):
-        """ Write a message. """
-        message = u"this is a test"
-        ajax_log(message)
-        content = open(os.path.join(settings.MEDIA_ROOT,"ajax_log.txt"), "r").read()
-        self.assertIn(message, content)
-        
+from core.bandcochon.models import Utilisateur
+from .models import Preference, Template, MailingList
+from . import notification_send
+
+
 class TestPreferenceModel(TestCase):
     def test_double_pref(self):
         self._create_pref()
         two = self._create_pref()
         self.assertEqual(two.pk, Preference.objects.get_default().pk)
-    
+
     def test_double_pref_with_default(self):
         one = self._create_pref()
         two = self._create_pref()
@@ -61,7 +38,7 @@ class TestPreferenceModel(TestCase):
         """ """
         pref = self._create_pref()
         self.assertEqual(pref.offuscate_pass(), u"*" * len(pref.password))
-        
+
     def test_default2string(self):
         """ """
         pref = self._create_pref()
@@ -69,7 +46,7 @@ class TestPreferenceModel(TestCase):
         pref.default_account = False
         pref.save()
         self.assertEqual(pref.default2string(), u"Non")
-        
+
     def _create_pref(self):
         """ Create a dummy preference """
         return Preference.objects.create(
@@ -84,24 +61,24 @@ class TestPreferenceModel(TestCase):
             default_account=True
         )
 
+
 class TestThatNeedServer(TestCase):
-    pass
-#    def setUp(self):
-#        self.server = ThreadedServer()
-#        self.server.start()
-#        
-#    def tearDown(self):
-#        self.server.set_stop()
-        
-    
+    def setUp(self):
+        """ Mock the smtp library to control the email sending
+        """
+        smtplib.SMTP = minimock.Mock('smtplib.SMTP', tracker=None)
+        smtplib.SMTP.mock_returns = minimock.Mock('smtp_connection', tracker=None)
+
+
 class TestNotificationSend(TestThatNeedServer):
     """ Test notification object and shortcut
     """
+
     def test_no_preferences_sets_no_template(self):
         """ Test with no template and no preferences. """
         with self.assertRaises(Http404):
             notification_send("atest")
-        
+
     def _create_pref(self, name="test", webmaster="someone@example.com"):
         """ Create a dummy preference """
         Preference.objects.create(
@@ -122,39 +99,38 @@ class TestNotificationSend(TestThatNeedServer):
             subject="a subject",
             body="A body {{ testme }}"
         )
-        
+
     def test_preferences_sets_no_template(self):
         """ Test with wrong template name (not created in this case) """
         self._create_pref()
         self.assertFalse(notification_send("atest"))
-            
+
     def test_preferences_sets_template(self):
-        """ Send an email. """ 
+        """ Send an email. """
         self._create_pref()
         self._create_template()
         self.assertTrue(notification_send("atest"))
-    
+
     def test_with_dest(self):
         """ Test with a destinataire """
         self._create_pref()
         self._create_template()
         self.assertTrue(notification_send("atest", dest="hello@world.com"))
-        
+
     def test_with_context(self):
         """ """
         self._create_pref()
         self._create_template()
-        context = Context({ 'testme' : 'This is a test'})
+        context = Context({'testme': 'This is a test'})
         self.assertTrue(notification_send('atest', context=context))
-        
-    
+
     def test_account_name(self):
         """ Test multiple account """
         self._create_pref("first")
         self._create_pref("second", "somewhere@overtherainbow.com")
         self._create_template()
         self.assertTrue(notification_send('atest', account_name="second"))
-        
+
 
 class TestMailingList(TestThatNeedServer):
     def setUp(self):
@@ -168,57 +144,38 @@ class TestMailingList(TestThatNeedServer):
             port=1025,
             default_account=True
         )
-        
+
         Template.objects.create(
             resume="a test",
             shortcut='atest',
             subject="a subject",
             body="A body {{ testme }}"
         )
-        
+
         self.user1 = User.objects.create(username="user1", email="user1@user1.com")
         Utilisateur.objects.create(user=self.user1)
         user2 = User.objects.create(username="user2", email="user1@user2.com")
         Utilisateur.objects.create(user=user2)
-        
-        
+
     def test_send_to_nobody(self):
         """ Does nothing. Just saving """
         MailingList.objects.create(
             subject="This is a test",
             content="Helo ! This is a test {{ withvariable }}"
         )
-        
+
     def test_send_to_all(self):
-        """ Send to all users. Please take a look to Debug SMTPd server """
+        """ Send to all users """
         MailingList.objects.create(
             subject="This is a test",
             content="Helo ! This is a test {{ withvariable }}",
             send_to_all=True
         )
-        
+
     def test_send_to_inactive(self):
         """ Send to inactive """
         MailingList.objects.create(
             subject="This is a test",
             content="Helo ! This is a test {{ withvariable }}",
             send_to_inactive=True
-        )        
-        
-    def test_send_to_active(self):
-        """ Send to active users """
-#        tship = TownShip.objects.create(name="testship")
-#        town = Town.objects.create(name="testtown", township=tship, added_by=self.user1)
-#        place = Place.objects.create(name="testplace", town=town, added_by=self.user1)
-#        Picture.objects.create(user=self.user1,
-#                               submission_date=datetime.datetime.now(),
-#                               township=tship,
-#                               town=town,
-#                               place=place)
-#        MailingList.objects.create(
-#            subject="This is a test",
-#            content="Helo ! This is a test {{ withvariable }}",
-#            send_to_active=True
-#        )
-
-
+        )
