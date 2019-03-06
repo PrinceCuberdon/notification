4e68815a1993a7c229c65f0f9db3fe6f2b7d25cb
Regis FLORET <regis@pc-regis.home.lan>
2019-03-06 20:39:12 +0100

    Fix email broken

--- __init__.py
+++ __init__.py
@@ -2,50 +2,29 @@
 # notification is part of Band Cochon
 # Band Cochon (c) Prince Cuberdon 2011 and Later <princecuberdon@bandcochon.fr>
 
-
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import logging
 
 from django import template
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import Http404
 from django.conf import settings
-from django.core.validators import validate_email, ValidationError
+from django.core.mail import send_mail
+from libs.notification.models import Template
 
 L = logging.getLogger("notification")
 
 
-def notification_send(template_shortcut, dest=None, context=None, account_name=None):
-    """ Send an email based on a shortcut """
-    from .models import Preference
+def notification_send(template_shortcut, dest=None, context=None):
+    """
+    Send an email based on a shortcut
+    """
+    if settings.IS_LOCAL:
+        return True
 
-    try:
-        if account_name is not None:
-            pref = Preference.objects.get(name=account_name)
-        else:
-            pref = Preference.objects.get_default()
-
-    except ObjectDoesNotExist:
-        L.error(u"notification.notification_send: The account_name '{name}' is not found".format(name=account_name))
-        raise Http404()
-
-    if dest is None:
-        dest = pref.webmaster
     try:
         notif = Notification(template_shortcut)
         notif.push(dest, context)
         notif.send()
 
         return True
-
-    except ObjectDoesNotExist as error:
-        L.error(u"notification.notification_send: {error}".format(error=error))
-
-    except smtplib.SMTPException as error:
-        """ Can't send the email """
-        L.error(u"notification.notification_send: {error}".format(error=error))
 
     except Exception as error:
         L.error(u"notification.notification_send : {error}".format(error=error))
@@ -58,29 +37,13 @@ class NotificationError(Exception):
 
 
 class Notification(object):
-    def __init__(self, template_shortcut=None, debug=False):
-        from .models import Preference
+    def __init__(self, template_shortcut=None):
 
         self.notif = []
         self.shortcut = template_shortcut
-        self.pref = Preference.objects.get_default()
-        if not debug:
-            self.connection = smtplib.SMTP(self.pref.server, int(self.pref.port))
-            if not self.pref.anonymous:
-                self.connection.login(self.pref.username, self.pref.password)
-            else:
-                self.connection.helo()
-        else:
-            self.connection = None
 
         self.subject = None
         self.body = None
-
-    def __del__(self):
-        try:
-            self.connection.quit()
-        except AttributeError:
-            pass
 
     def push(self, dest, context=None, shortcut=None):
         """ Push notifications in a stack """
@@ -88,7 +51,8 @@ class Notification(object):
 
         if sc is None:
             if self.subject is None or self.body is None:
-                L.error("notification.Notification.push: Subject or body not set")
+                L.error("notification.Notification.push: "
+                        "Subject or body not set")
                 raise NotificationError("Subject or body not set")
 
             subject = self.subject
@@ -108,27 +72,19 @@ class Notification(object):
         Do nothing on debug mode
         """
         if settings.IS_LOCAL or debug:
+            L.debug("We are in local. Go back")
             return
 
         for notif in self.notif:
-            # Do HTML text mail
-            msg = MIMEMultipart('alternative')
-            msg['From'] = self.pref.sendmail
-            msg['To'] = notif['dest']
-            msg['Subject'] = notif['subject']
-            htmlpart = MIMEText('<html><body>' + notif['body'] + '</body></html>', 'html', 'utf-8')
-            msg.attach(htmlpart)
-
-            if not debug:
-                try:
-                    # Ensure email is a valid one
-                    validate_email(notif['dest'])
-                    self.connection.sendmail(self.pref.sendmail, notif['dest'], msg.as_string())
-                except ValidationError as e:
-                    L.error(u"notification.Notification.send: Unable to send a emain. Reasons: {error}".format(error=e))
+            send_mail(subject=notif['subject'],
+                      recipient_list=[notif['dest']],
+                      message=notif['body'],
+                      html_message='<html><body>%s</body></html>' %
+                                   notif['body'],
+                      from_email=settings.EMAIL_SENDER,
+                      fail_silently=True)
 
     def render_template(self, template_shortcut, context):
-        from .models import Template
 
         temp = Template.objects.get(shortcut=template_shortcut)
         body = u"%s" % temp.body
--- tests.py
+++ tests.py
@@ -12,53 +12,10 @@ import smtplib
 from django.test import TestCase
 from django.http import Http404
 from django.template import Context
-from django.contrib.auth.models import User
 import minimock
 
-from core.bandcochon.models import Utilisateur
-from .models import Preference, Template, MailingList
+from .models import Template
 from . import notification_send
-
-
-class TestPreferenceModel(TestCase):
-    def test_double_pref(self):
-        self._create_pref()
-        two = self._create_pref()
-        self.assertEqual(two.pk, Preference.objects.get_default().pk)
-
-    def test_double_pref_with_default(self):
-        one = self._create_pref()
-        two = self._create_pref()
-        two.default_account = False
-        two.save()
-        self.assertEqual(one.pk, Preference.objects.get_default().pk)
-
-    def test_password_offuscation(self):
-        """ """
-        pref = self._create_pref()
-        self.assertEqual(pref.offuscate_pass(), u"*" * len(pref.password))
-
-    def test_default2string(self):
-        """ """
-        pref = self._create_pref()
-        self.assertEqual(pref.default2string(), u"Oui")
-        pref.default_account = False
-        pref.save()
-        self.assertEqual(pref.default2string(), u"Non")
-
-    def _create_pref(self):
-        """ Create a dummy preference """
-        return Preference.objects.create(
-            name="test",
-            webmaster="someone@exemple.com",
-            sendmail="someone-else@example.com",
-            anonymous=False,
-            username="testme",
-            password="testmeagain",
-            server="localhost",
-            port=1025,
-            default_account=True
-        )
 
 
 class TestThatNeedServer(TestCase):
@@ -66,7 +23,8 @@ class TestThatNeedServer(TestCase):
         """ Mock the smtp library to control the email sending
         """
         smtplib.SMTP = minimock.Mock('smtplib.SMTP', tracker=None)
-        smtplib.SMTP.mock_returns = minimock.Mock('smtp_connection', tracker=None)
+        smtplib.SMTP.mock_returns = minimock.Mock('smtp_connection',
+                                                  tracker=None)
 
 
 class TestNotificationSend(TestThatNeedServer):
@@ -77,18 +35,6 @@ class TestNotificationSend(TestThatNeedServer):
         """ Test with no template and no preferences. """
         with self.assertRaises(Http404):
             notification_send("atest")
-
-    def _create_pref(self, name="test", webmaster="someone@example.com"):
-        """ Create a dummy preference """
-        Preference.objects.create(
-            name=name,
-            webmaster=webmaster,
-            sendmail="someone-else@example.com",
-            anonymous=True,
-            server="localhost",
-            port=1025,
-            default_account=True
-        )
 
     def _create_template(self):
         """ Create a dummy template """
@@ -101,82 +47,26 @@ class TestNotificationSend(TestThatNeedServer):
 
     def test_preferences_sets_no_template(self):
         """ Test with wrong template name (not created in this case) """
-        self._create_pref()
         self.assertFalse(notification_send("atest"))
 
     def test_preferences_sets_template(self):
         """ Send an email. """
-        self._create_pref()
         self._create_template()
         self.assertTrue(notification_send("atest"))
 
     def test_with_dest(self):
         """ Test with a destinataire """
-        self._create_pref()
         self._create_template()
         self.assertTrue(notification_send("atest", dest="hello@world.com"))
 
     def test_with_context(self):
         """ """
-        self._create_pref()
         self._create_template()
         context = Context({'testme': 'This is a test'})
         self.assertTrue(notification_send('atest', context=context))
 
     def test_account_name(self):
         """ Test multiple account """
-        self._create_pref("first")
-        self._create_pref("second", "somewhere@overtherainbow.com")
         self._create_template()
-        self.assertTrue(notification_send('atest', account_name="second"))
+        self.assertTrue(notification_send('atest',))
 
-
-class TestMailingList(TestThatNeedServer):
-    def setUp(self):
-        """ Create two users, mail preference and a template """
-        Preference.objects.create(
-            name="test",
-            webmaster="me@myself.and.i",
-            sendmail="someone-else@example.com",
-            anonymous=True,
-            server="localhost",
-            port=1025,
-            default_account=True
-        )
-
-        Template.objects.create(
-            resume="a test",
-            shortcut='atest',
-            subject="a subject",
-            body="A body {{ testme }}"
-        )
-
-        self.user1 = User.objects.create(username="user1", email="user1@user1.com")
-        Utilisateur.objects.create(user=self.user1)
-        user2 = User.objects.create(username="user2", email="user1@user2.com")
-        Utilisateur.objects.create(user=user2)
-
-        super(TestMailingList, self).setUp()
-
-    def test_send_to_nobody(self):
-        """ Does nothing. Just saving """
-        MailingList.objects.create(
-            subject="This is a test",
-            content="Helo ! This is a test {{ withvariable }}"
-        )
-
-    def test_send_to_all(self):
-        """ Send to all users """
-        MailingList.objects.create(
-            subject="This is a test",
-            content="Helo ! This is a test {{ withvariable }}",
-            send_to_all=True
-        )
-
-    def test_send_to_inactive(self):
-        """ Send to inactive """
-        MailingList.objects.create(
-            subject="This is a test",
-            content="Helo ! This is a test {{ withvariable }}",
-            send_to_inactive=True
-        )
